71eb558c9606186d485e00bd17416c6d98f11848
nstarman <nstarman@users.noreply.github.com>
2024-12-08 16:44:47 -0500

    🔨: change to hatch
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/quantity/__init__.py
+++ src/quantity/__init__.py
@@ -5,7 +5,7 @@ Copyright (c) 2024 Astropy Developers. All rights reserved.
 
 from . import api
 from ._src import Quantity
-from .version import version as __version__  # noqa: F401
+from ._version import version as __version__  # noqa: F401
 
 __all__ = [
     # modules
